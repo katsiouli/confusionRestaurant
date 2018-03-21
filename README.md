6cf6714ba6644948cdfad14f9a34ce222c4ff77d
Polina Katsiouli <polyxeni.katsiouli@intrasoft-intl.com>
2018-03-21 18:02:09 +0200

    aaded missing config files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Confusion
+# Project1
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.0.0.
 
--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,4 +1,8 @@
 export class Dish {
   name: string;
-  
+  image: string;
+  category: string;
+  price: string;
+  label: string;
+  description: string;
 }
