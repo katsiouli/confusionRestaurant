81e8046a3d55ad5303dd54bef781f2166df8b135
Polina Katsiouli <polyxeni.katsiouli@intrasoft-intl.com>
2018-03-28 18:06:27 +0300

    data binding

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,5 @@
+import { Comment } from './comment';
+
 export class Dish {
   name: string;
   image: string;
@@ -5,4 +7,5 @@ export class Dish {
   price: string;
   label: string;
   description: string;
+  comments: Comment[];
 }
