eda2e57edba88d09fed1f19c58f228cf6ca1653a
Polina Katsiouli <polyxeni.katsiouli@intrasoft-intl.com>
2018-04-05 13:25:10 +0300

    basic services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +33,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
